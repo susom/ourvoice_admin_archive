fe1ae2547c2a2b47d7b6943d64e94422c917e209
Irvin Szeto <irvins@stanford.edu>
2017-04-26 15:49:16 -0700

    single deletion with confirmation

--- summary.php
+++ summary.php
@@ -6,42 +6,11 @@ require_once "common.php";
 
 date_default_timezone_set('America/Los_Angeles');
 
-//$couch_proj     	= cfg::$couch_proj_db;
-//$couch_db 	    = cfg::$couch_config_db;
 $couch_url 	    	= cfg::$couch_url . "/" . cfg::$couch_proj_db . "/" . cfg::$couch_config_db;
-//$couch_user 		= cfg::$couch_user;
-//$couch_pw 	    = cfg::$couch_pw;
-
-//$_ENV['couch_url'   	] 	='https://ourvoice-cdb.med.stanford.edu'			;
-//$_ENV['couch_proj_proj' ] 	='disc_projects';
-//$_ENV['couch_config_db'  	] 	='all_projects';
-//$_ENV['couch_users_db']  	='disc_users';
-//$_ENV['couch_all_db' 	] 	='_all_docs';
-//$_ENV['couch_user'   	] 	='disc_user_general';
-//$_ENV['couch_pw'    	] 	="rQaKibbDx7rP";
-//$_ENV['gmaps_key'		] 	="AIzaSyCn-w3xVV38nZZcuRtrjrgy4MUAW35iBOo";
 
 // FIRST GET THE PROJECT DATA
 if( empty($_SESSION["DT"]) ){
-//	$couch_proj = cfg::$couch_proj_db;
-//	$couch_db 	= $_ENV["couch_config_db"];
-//	$couch_url 	= $_ENV["couch_url"] . "/$couch_proj" . "/$couch_db";
-//	$couch_user 	= $_ENV["couch_user"];
-//	$couch_pw 	= $_ENV["couch_pw"];
-
-	//CURL OPTIONS
-//	$ch 		= curl_init($couch_url);
-//	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//		"Content-type: application/json",
-//		"Accept: */*"
-//	));
-//	curl_setopt($ch, CURLOPT_USERPWD, "$couch_user:$couch_pw");
-//	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); //JUST FETCH DATA
-
 	$response 	= doCurl($couch_url);
-//	curl_exec($ch);
-//	curl_close($ch);
 
 	//TURN IT INTO PHP ARRAY
 	$_SESSION["DT"] = json_decode(stripslashes($response),1);
@@ -55,20 +24,21 @@ $projs 				= $ap["project_list"];
 $projects 			= [];
 $active_project_id 	= null;
 
-if(isset($_POST["for_delete"])){
-	$fordelete 	= [];
-	foreach($_POST["for_delete"] as $i=> $row){
-		$temp 	= explode("|",$row);
-		array_push($fordelete,array(
-			 "_id" 		=> $temp[0]
-			,"_rev" 	=> $temp[1]
-			,"_deleted" => true
+if(isset($_POST["for_delete"]) && $_POST["for_delete"]){
+	$_id 	= $_POST["doc_id"];
+	$_rev 	= $_POST["rev"];
+
+	$fordelete      = [];
+	array_push($fordelete, array(
+	         "_id"          => $_id
+	        ,"_rev"         => $_rev
+	        ,"_deleted" => true
 		));
-	}
 
 	// Bulk update docs
     $couch_url 	= cfg::$couch_url . "/" . cfg::$couch_users_db . "/_bulk_docs";
     $response 	= doCurl($couch_url, json_encode(array("docs" => $fordelete)), "POST");
+	exit;
 }
 
 //NOW LOGIN TO YOUR PROJECT
@@ -96,33 +66,6 @@ if(isset($_POST["proj_id"]) && isset($_POST["proj_pw"])){
 <body id="main">
 <?php
 if( $active_project_id ){
-//
-//
-//	$couch_proj = $_ENV["couch_users_db"];
-//	$couch_db 	= $_ENV["couch_all_db"];
-//	$limit 		= 10;
-//	$offset 	= 0; //offset
-//	$qs 		= "?include_docs=true";//&limit=$limit&skip=$offset
-//
-//	$couch_base = $_ENV["couch_url"];
-//	$couch_url 	= $couch_base. "/$couch_proj" ."/$couch_db" .$qs;
-//	$couch_user 	= $_ENV["couch_user"];
-//	$couch_pw 	= $_ENV["couch_pw"];
-//	// $couch_url  = "https://ourvoice-cdb.med.stanford.edu/disc_users/_design/hasphotos/_view/all";
-//
-//	//CURL OPTIONS
-//	$ch 		= curl_init($couch_url);
-//	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//		"Content-type: application/json",
-//		"Accept: */*"
-//	));
-//	curl_setopt($ch, CURLOPT_USERPWD, "$couch_user:$couch_pw");
-//	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); //JUST FETCH DATA
-//
-//	$response 	= curl_exec($ch);
-//	curl_close($ch);
-
 	// Build query string
 	$qs = http_build_query(array(
 	        'include_docs' => 'true'
@@ -154,7 +97,6 @@ if( $active_project_id ){
 	krsort($active_project);
 
 	echo "<form id='project_summary' method='post'>";
-	echo "<input id='delete_all' type='submit' value='Delete Checked Data Entries'></input>";
 	echo "<input type='hidden' name='proj_id' value='".$_POST["proj_id"]."'/>";
 	echo "<input type='hidden' name='proj_pw' value='".$_POST["proj_pw"]."'/>";
 
@@ -187,7 +129,7 @@ if( $active_project_id ){
 		echo "<div id='google_map_$i' class='gmap'></div>";
 		
 		echo "<section class='photo_previews'>";
-		echo "<label><input type='checkbox' name='for_delete[]' value='".$doc["_id"]."|".$doc["_rev"]."'/>Delete This Walk</label>";
+		echo "<a href='#' class='btn btn-danger deletewalk' data-id='".$doc["_id"]."' data-rev='".$doc["_rev"]."'/>Delete This Walk</a>";
 		echo "<h5>Photo Previews</h5>";
 		echo "<div class='thumbs'>";
 		echo "<ul>";
@@ -369,9 +311,28 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$("#project_summary").submit(function(){
-		var yn = confirm("You will delete this entry and all photos/audio/survey answers associated with it.  Click OK to continue.");
-		return yn;
+	$(".deletewalk").click(function(e){
+		e.preventDefault();
+		var _id 	= $(this).data("id");
+		var last4 	= _id.substr(_id.length - 4);
+		var _rev 	= $(this).data("rev");
+
+		var _parent	= $(this).closest(".user_entry");
+
+		var confirm = prompt("Deleting this walk will also delete all photos, audio, maps and survey data attached to it.  To confirm deletion type in the last 4 digits of the walk ID");
+		if(confirm == last4){
+			//AJAX DELETE IT
+			$.ajax({
+			  type 		: "POST",
+			  url 		: "summary.php",
+			  data 		: { doc_id: _id, rev: _rev , for_delete: true},
+			}).done(function(response) {
+				_parent.slideUp("medium");
+			}).fail(function(msg){
+				// console.log("rotation save failed");
+			});
+		}
+		return false;
 	});
 });
 </script>
