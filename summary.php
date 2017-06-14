cb2c5a98cab0f2e3ade5409552a7cb2702bbf2ac
Irvin Szeto <irvins@stanford.edu>
2017-06-13 19:04:03 -0700

    yes! ok so now only load the project, the most recent day, and the others load on demand

--- summary.php
+++ summary.php
@@ -2,7 +2,132 @@
 require_once "common.php";
 // session_destroy();
 // exit;
-$couch_url = cfg::$couch_url . "/" . cfg::$couch_proj_db . "/" . cfg::$couch_config_db;
+function filter_by_projid($view, $keys_array){
+	$qs 		= http_build_query(array( 'key' => $keys_array ));
+    $couch_url 	= cfg::$couch_url . "/" . cfg::$couch_users_db . "/" . "_design/filter_by_projid/_view/".$view."?" .  $qs;
+    $response 	= doCurl($couch_url);
+    return json_decode($response,1);
+}
+
+function printRow($doc){
+	global $project_meta;
+
+	$codeblock 	= array();
+	$i 			= $doc["_id"];
+	$photos 	= $doc["photos"];
+	$geotags 	= $doc["geotags"];
+	$survey 	= $doc["survey"];
+	$_attach 	= !empty($doc["_attachments"]) ? $doc["_attachments"] : null;
+	$forjsongeo = array();
+	
+	// filter out low accuracy
+	$forjsongeo = array_filter($geotags,function($tag){
+		return $tag["accuracy"] <= 50;
+	});
+	$json_geo 	= json_encode($forjsongeo);
+
+	$codeblock[] = "<div class='user_entry'>";
+	$codeblock[] = "<hgroup>";
+	$codeblock[] = "<h4>(".$doc["lang"] .") : 
+	<b>".date("F j, Y", floor($doc["geotags"][0]["timestamp"]/1000))."</b> 
+	<i>".$doc["_id"]."</i></h4>";
+	$codeblock[] = "</hgroup>";
+	$codeblock[] = "<div id='google_map_$i' class='gmap'></div>";
+	
+	$codeblock[] = "<section class='photo_previews'>";
+	$codeblock[] = "<a href='#' class='btn btn-danger deletewalk' data-id='".$doc["_id"]."' data-rev='".$doc["_rev"]."'>Delete This Walk</a>";
+	$codeblock[] = "<h5>Photo Previews</h5>";
+	$codeblock[] = "<div class='thumbs'>";
+	$codeblock[] = "<ul>";
+	foreach($photos as $n => $photo){
+		if(is_null($photo)){
+			continue;
+		}
+		$hasaudio 	= !empty($photo["audio"]) ? "has" : "";
+		$long 		= $photo["geotag"]["longitude"];
+		$lat 		= $photo["geotag"]["latitude"];
+		$timestamp  = $photo["geotag"]["timestamp"];
+
+		$goodbad 	= "";
+		if($photo["goodbad"] > 1){
+			$goodbad  .= "<span class='goodbad good'></span>";
+		}
+
+		if($photo["goodbad"] == 1 || $photo["goodbad"] == 3){
+			$goodbad  .= "<span class='goodbad bad'></span>";
+		}
+
+		$rotate 	= isset($photo["rotate"]) ? $photo["rotate"] : 0;
+		$photo_name = "photo_".$n.".jpg";
+
+		// $photo_uri 	= $couch_base . "/" . $couch_proj . "/" . $doc["_id"] . "/" . $photo_name;
+		$photo_uri 	= "passthru.php?_id=".$doc["_id"]."&_file=$photo_name";
+		$detail_url = "photo.php?_id=".$doc["_id"]."&_file=$photo_name";
+
+		$attach_url = "#";
+		$audio_attachments = "";
+		if(!empty($photo["audio"])){
+			$num_audios = intval($photo["audio"]);
+			$num 		= $num_audios > 1 ? "<span>x$num_audios</span>" :"";
+			$audio_attachments .= "<a class='audio $hasaudio'></a> $num";
+		}
+		$codeblock[] = "<li id='photo_$n'>
+		<figure>
+		<a href='$detail_url' target='_blank' rel='google_map_$i' data-photo_i=$n data-doc_id='".$doc["_id"]."' data-long='$long' data-lat='$lat' class='preview rotate' rev='$rotate'><img src='$photo_uri' /><span></span><b></b></a>
+		<figcaption>
+			<span class='time'>@".date("g:i a", floor($timestamp/1000))."</span>
+			".$goodbad."
+			".$audio_attachments."
+		</figcaption>
+		</figure></li>";
+	}
+	$codeblock[] = "</ul>";
+	$codeblock[] = "</div>";
+	$codeblock[] = "</section>";
+
+	$codeblock[] = "<section class='survey_response'>";
+	$codeblock[] = "<h5>Survey Responses</h5>";
+	$codeblock[] = "<div class='survey'>";
+	if(empty($survey)){
+		$codeblock[] = "<p><i>No Survey Responses</i></p>";
+	}
+
+	//WHOOO THIS IS NOT GREAT
+	$tempsurvey = array();
+	foreach($project_meta["surveys"] as $s){
+		$tempoptions = array();
+		if(isset($s["options"])){
+			foreach($s["options"] as $o){
+				$tempoptions[$o["value"]] = $o["en"]; 
+			}
+		}else{
+			$tempoptions = null;
+		}
+		$tempsurvey[$s["name"]] = array(
+				"label" => $s["label"]["en"]
+				,"options" =>  $tempoptions
+			);
+	}
+
+	$unique = array();
+	foreach($survey as $q){
+		$unique[$q["name"]] = $q["value"];
+	}
+	$codeblock[] = "<ul>";
+	foreach($unique as $name => $value){
+		$v = (!empty($tempsurvey[$name]["options"]))  ?  $tempsurvey[$name]["options"][$value] :$value;
+		$codeblock[] = "<li><i>".$tempsurvey[$name]["label"]."</i> : <b>$v</b></li>";
+	}
+	$codeblock[] = "</ul>";
+	$codeblock[] = "</div>";
+	$codeblock[] = "</section>";
+	$codeblock[] = "</div>";
+	$codeblock[] = "<script>$(document).ready(function(){ drawGMap($json_geo, '$i');\n  });</script>";
+	return $codeblock;
+}
+
+
+
 
 if( empty($_SESSION["DT"]) ){
 	// FIRST GET THE PROJECT DATA
@@ -21,6 +146,26 @@ $active_project_id 	= null;
 $active_pid 		= null;
 $alerts 			= array();
 
+//AJAX GETTING DAY'S DATA
+if(isset($_POST["active_pid"]) && $_POST["date"]){
+	$active_pid 	= $_POST["active_pid"];
+	$date 			= $_POST["date"];
+	$project_meta 	= $ap["project_list"][$active_pid];
+
+	$response 		= filter_by_projid("get_data_day","[\"$active_pid\",\"$date\"]");
+	$days_data 		= rsort($response["rows"]); 
+	
+	$code_block 	= array();
+	foreach($response["rows"] as $row){
+		$doc 		= $row["value"];
+		$code_block = array_merge($code_block, printRow($doc));
+	}
+
+	echo implode("",$code_block);
+	exit;
+}
+
+//AJAX DELETEING DATA ENTRY
 if(isset($_POST["for_delete"]) && $_POST["for_delete"]){
 	$_id 	= $_POST["doc_id"];
 	$_rev 	= $_POST["rev"];
@@ -84,127 +229,6 @@ h4[data-toggle="collapse"]{
 </head>
 <body id="main">
 <?php
-function printRow($doc){
-	global $project_meta;
-
-	$i 			= $doc["_id"];
-	$photos 	= $doc["photos"];
-	$geotags 	= $doc["geotags"];
-	$survey 	= $doc["survey"];
-	$_attach 	= !empty($doc["_attachments"]) ? $doc["_attachments"] : null;
-	$forjsongeo = array();
-	
-	// filter out low accuracy
-	$forjsongeo = array_filter($geotags,function($tag){
-		return $tag["accuracy"] <= 50;
-	});
-	$json_geo 	= json_encode($forjsongeo);
-
-	echo "<div class='user_entry'>";
-	echo "<hgroup>";
-	echo "<h4>(".$doc["lang"] .") : 
-	<b>".date("F j, Y", floor($doc["geotags"][0]["timestamp"]/1000))."</b> 
-	<i>".$doc["_id"]."</i></h4>";
-	echo "</hgroup>";
-	echo "<div id='google_map_$i' class='gmap'></div>";
-	
-	echo "<section class='photo_previews'>";
-	echo "<a href='#' class='btn btn-danger deletewalk' data-id='".$doc["_id"]."' data-rev='".$doc["_rev"]."'/>Delete This Walk</a>";
-	echo "<h5>Photo Previews</h5>";
-	echo "<div class='thumbs'>";
-	echo "<ul>";
-	foreach($photos as $n => $photo){
-		if(is_null($photo)){
-			continue;
-		}
-		$hasaudio 	= !empty($photo["audio"]) ? "has" : "";
-		$long 		= $photo["geotag"]["longitude"];
-		$lat 		= $photo["geotag"]["latitude"];
-		$timestamp  = $photo["geotag"]["timestamp"];
-
-		$goodbad 	= "";
-		if($photo["goodbad"] > 1){
-			$goodbad  .= "<span class='goodbad good'></span>";
-		}
-
-		if($photo["goodbad"] == 1 || $photo["goodbad"] == 3){
-			$goodbad  .= "<span class='goodbad bad'></span>";
-		}
-
-		$rotate 	= isset($photo["rotate"]) ? $photo["rotate"] : 0;
-		$photo_name = "photo_".$n.".jpg";
-
-		// $photo_uri 	= $couch_base . "/" . $couch_proj . "/" . $doc["_id"] . "/" . $photo_name;
-		$photo_uri 	= "passthru.php?_id=".$doc["_id"]."&_file=$photo_name";
-		$detail_url = "photo.php?_id=".$doc["_id"]."&_file=$photo_name";
-
-		$attach_url = "#";
-		$audio_attachments = "";
-		if(!empty($photo["audio"])){
-			$num_audios = intval($photo["audio"]);
-			$num 		= $num_audios > 1 ? "<span>x$num_audios</span>" :"";
-			$audio_attachments .= "<a class='audio $hasaudio'></a> $num";
-		}
-		echo "<li id='photo_$n'>
-		<figure>
-		<a href='$detail_url' target='_blank' rel='google_map_$i' data-photo_i=$n data-doc_id='".$doc["_id"]."' data-long='$long' data-lat='$lat' class='preview rotate' rev='$rotate'><img src='$photo_uri' /><span></span><b></b></a>
-		<figcaption>
-			<span class='time'>@".date("g:i a", floor($timestamp/1000))."</span>
-			".$goodbad."
-			".$audio_attachments."
-		</figcaption>
-		</figure></li>";
-	}
-	echo "</ul>";
-	echo "</div>";
-	echo "</section>";
-
-	echo "<section class='survey_response'>";
-	echo "<h5>Survey Responses</h5>";
-	echo "<div class='survey'>";
-	if(empty($survey)){
-		echo "<p><i>No Survey Responses</i></p>";
-	}
-
-	//WHOOO THIS IS NOT GREAT
-	$tempsurvey = array();
-	foreach($project_meta["surveys"] as $s){
-		$tempoptions = array();
-		if(isset($s["options"])){
-			foreach($s["options"] as $o){
-				$tempoptions[$o["value"]] = $o["en"]; 
-			}
-		}else{
-			$tempoptions = null;
-		}
-		$tempsurvey[$s["name"]] = array(
-				"label" => $s["label"]["en"]
-				,"options" =>  $tempoptions
-			);
-	}
-
-	$unique = array();
-	foreach($survey as $q){
-		$unique[$q["name"]] = $q["value"];
-	}
-	echo "<ul>";
-	foreach($unique as $name => $value){
-		$v = (!empty($tempsurvey[$name]["options"]))  ?  $tempsurvey[$name]["options"][$value] :$value;
-		echo "<li><i>".$tempsurvey[$name]["label"]."</i> : <b>$v</b></li>";
-	}
-	echo "</ul>";
-	echo "</div>";
-	echo "</section>";
-	echo "</div>";
-	echo "<script>$(document).ready(function(){ drawGMap($json_geo, '$i');\n  });</script>";
-}
-
-function filter_by_projid($view, $keys_array){
-	$qs 		= http_build_query(array( 'key' => $keys_array ));
-    $couch_url 	= cfg::$couch_url . "/" . cfg::$couch_users_db . "/" . "_design/filter_by_projid/_view/".$view."?" .  $qs;
-    $response 	= doCurl($couch_url);
-    return json_decode($response,1);
-}
 // $response = filter_by_projid("all","[\"$active_pid\",\"IRV_1BD92AE2-718C-497E-8B48-47C4B7F3BA39_1_1495147319092\"]");
 
 if( $active_project_id ){
@@ -228,47 +252,39 @@ if( $active_project_id ){
 	echo "<form id='project_summary' method='post'>";
 	echo "<input type='hidden' name='proj_id' value='".$_POST["proj_id"]."'/>";
 	echo "<input type='hidden' name='proj_pw' value='".$_POST["proj_pw"]."'/>";
-	$gmaps 				= array();
 	$project_meta 		= $ap["project_list"][$active_pid];
-
 	$most_recent_date 	= true;
 	foreach($date_headers as $date => $record_count){
 		if($most_recent_date){
-			echo "<section>";
-			echo "<h4 data-toggle='collapse' data-target='#day_$date'>$date</h4>";
+			echo "<aside>";
+			echo "<h4 class='day' rel='true' rev='$active_pid' data-toggle='collapse' data-target='#day_$date'>$date</h4>";
 			echo "<div id='day_$date' class='collapse in'>";
 			
 			//AUTOMATICALLY SHOW MOST RECENT DATE's DATA, AJAX THE REST
 			$response 	= filter_by_projid("get_data_day","[\"$active_pid\",\"$date\"]");
 			$days_data 	= rsort($response["rows"]); 
 
+			$code_block = array();
 			foreach($response["rows"] as $row){
-				$doc = $row["value"];
-				printRow($doc);
+				$doc 		= $row["value"];
+				$code_block = array_merge($code_block,printRow($doc));
 			}
+			echo implode("",$code_block);
 			echo "</div>";
-			echo "</section>";
+			echo "</aside>";
 
 			$most_recent_date = false;
 			continue;
 		}
 
 		//SHOW THE HEADERS OF ALL THE OTHER ONES
-		echo "<section>";
-		echo "<h4 data-toggle='collapse' data-target='#day_$date'>$date</h4>";
+		echo "<aside>";
+		echo "<h4 class='day' rel='false' rev='$active_pid' data-toggle='collapse' data-target='#day_$date'>$date</h4>";
 		echo "<div id='day_$date' class='collapse'>";
 		
-		//CHANGE THESE TO AJAX LATER 
-		$response 	= filter_by_projid("get_data_day","[\"$active_pid\",\"$date\"]");
-		$days_data 	= rsort($response["rows"]); 
-
-		foreach($response["rows"] as $row){
-			$doc = $row["value"];
-			printRow($doc);
-		}
-
+		echo "<img class='loading' style='margin:0 auto; display:block;' src='https://s-media-cache-ak0.pinimg.com/originals/e0/f5/a5/e0f5a5f8c2e378df4fddd75e26e9a5a3.gif'/>";
 		echo "</div>";
-		echo "</section>";
+		echo "</aside>";
 	}
 	echo "</form>";
 }else{
@@ -320,18 +336,54 @@ function addmarker(latilongi,map_id) {
 
 $(document).ready(function(){
 	window.current_preview = null;
-	$(".preview").hover(function(){
-		var long 	= $(this).data("long");
-		var lat 	= $(this).data("lat"); 
-		var map_id 	= $(this).attr("rel");
-		var latlng 	= new google.maps.LatLng(lat, long);
 
-		addmarker(latlng,map_id);
-	},function(){
-		current_preview.setMap(null);
+	//COLLAPSING AJAX DATE HEADER
+	$("h4.day").on("click",function(){
+		var hasData 	= $(this).attr("rel");
+		var active_pid 	= $(this).attr("rev");
+		var date 		= $(this).text();
+		var target 		= $(this).data("target");
+
+		if(hasData == "false"){
+			$.ajax({
+			  type 		: "POST",
+			  url 		: "summary.php",
+			  data 		: { active_pid: active_pid, date: date },
+			}).done(function(response) {
+				// console.log(response);
+				setTimeout(function(){
+					$(target).find(".loading").fadeOut("fast",function(){
+						$(this).remove() });
+				},1500);
+				setTimeout(function(){
+					$(target).append(response);
+				},1600);
+			}).fail(function(msg){
+				// console.log("rotation save failed");
+			});
+
+			//flip flag
+			$(this).attr("rel","true");
+		}
 	});
 
-	$(".preview span").click(function(){
+	//HOVER ON MAP SPOT
+	$(document).on({
+	    mouseenter: function () {
+	        var long 	= $(this).data("long");
+			var lat 	= $(this).data("lat"); 
+			var map_id 	= $(this).attr("rel");
+			var latlng 	= new google.maps.LatLng(lat, long);
+
+			addmarker(latlng,map_id);
+	    },
+	    mouseleave: function () {
+	        current_preview.setMap(null);
+	    }
+	}, ".preview");
+
+	//ROTATE
+	$(".collapse").on("click",".preview span",function(){
 		var rotate = $(this).parent().attr("rev");
 		if(rotate < 3){
 			rotate++;
@@ -354,7 +406,9 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$(".preview b").click(function(){
+	//DELETE PHOTO
+	$(".collapse").on("click", ".preview b", function(){
+		console.log("hey shithead");
 		var doc_id 	= $(this).parent().data("doc_id");
 		var photo_i = $(this).parent().data("photo_i"); 
 		
@@ -375,7 +429,8 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$(".deletewalk").click(function(e){
+	//DELTEE WALK
+	$(".collapse").on("click",".deletewalk",function(e){
 		e.preventDefault();
 		var _id 	= $(this).data("id");
 		var last4 	= _id.substr(_id.length - 4);
