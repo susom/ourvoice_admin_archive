95cb3009ac09402ccdb5fa4dabb41789e9342d6b
jschultz <jschultz@DN0a22f1e9.SUNet>
2018-02-28 11:52:27 -0800

    added folders finally to index ; working

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 require_once "common.php";
 
+if(!isset($ALL_PROJ_DATA)){
+	$turl  = cfg::$couch_url . "/" . cfg::$couch_users_db . "/"  . "_design/filter_by_projid/_view/get_data_ts"; 
+	$pdurl = cfg::$couch_url . "/" . cfg::$couch_proj_db . "/" . cfg::$couch_config_db;
+
+	$ALL_PROJ_DATA = urlToJson($pdurl); //might have to store this in a session variable
+	$_SESSION["DT"] = $ALL_PROJ_DATA;
+	//print_rr($ALL_PROJ_DATA);
+	$tm = urlToJson($turl); //Just for times + project abv
+	$stor = $listid = array();
+	$stor = parseTime($tm, $stor, $listid);
+
+	foreach ($stor as $key => $value)
+	  array_push($listid, $key);
+}
+
 if(isset($_GET["clearsession"])){
 	$_SESSION = null;
 }
@@ -125,10 +140,20 @@ if(isset($_POST["discpw"])){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+  	<meta charset="utf-8">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<!-- SCRIPTS HAVE TO BE IN -->
     <link href="css/dt_common.css?v=<?php echo time();?>" rel="stylesheet" type="text/css"/>
     <link href="css/dt_index.css?v=<?php echo time();?>" rel="stylesheet" type="text/css"/>
+  
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body id="main" class="configurator">
 <div id="box">
@@ -156,7 +181,7 @@ if(!isset($_SESSION["discpw"])) {
 			<copyright>© Stanford University 2017</copyright>
 			<disclaim>Please note that Discovery Tool data can be viewed only by signatories to the The Stanford Healthy Neighborhood Discovery Tool Software License Agreement and in accordance with all relevant IRB/Human Subjects requirements.</disclaim>
 			<label class="checkauth">
-				<input type="checkbox" name='authorized'>  Check here to indicate that you are authorized to view these data
+				<input type="checkbox" name='authorized'>  Check here to indicate that you are authorized to view this data
 			</label>
 			<label><input type="password" name="discpw" id="proj_pw" placeholder="Admin Password"/></label> 
 			<!--Sends entered text in as "discpw"= and authorized as on/empty-->
@@ -279,14 +304,49 @@ if(!isset($_SESSION["discpw"])) {
 }
 ?>
 </div>
-
+<!-- 
 <div id = "FolderArea">
 	<iframe src="config_gui.php" width = 55% height = 500></iframe>
-</div>
+</div> -->
 
 
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+<input type ="text" id = "foldername" style = "margin-left:20px;">
+    <button type ="button" onclick="CreateFolder(document.getElementById('foldername').value)">Create Folder</button>
+    <div id = "folderspace">
+
+      <?php
+        foreach ($ALL_PROJ_DATA["folders"] as $key => $value) { //populate folders inside working space
+          echo "<div class ='ui-widget-drop'><p>".$value." </p></div>";
+          echo "<div class ='hiddenFolders' id ='".$value."'>";
+            foreach ($ALL_PROJ_DATA["project_list"] as $k => $v) {
+              if(isset($v["dropTag"]) && $v["dropTag"] ==$value){
+               // echo '<div class="foldercontents" data-key = "'.$k.'" ><p>'.$v["project_id"] .'</p></div>';
+                echo '<div class="foldercontents" data-key = "'.$k.'" ><p><a href="index.php?proj_idx='.$k.'"'.'>'.$v["project_id"] .'</a></p></div>';
+                  
+
+              }
+            }
+
+          echo "</div>";
+        }
+
+      ?>    
+    </div>
+<div id = workingspace>
+      <?php
+      foreach ($ALL_PROJ_DATA["project_list"] as $key=>$projects) { //populate projects on base page
+          if(isset($ALL_PROJ_DATA["project_list"][$key]["dropTag"]))
+          {
+            //if droptag is set we want to store things in the individual folders.
+          }else
+            //echo '<div class="ui-widget-drag" data-key = "'.$key.'" ><p>'.$projects["project_id"] .'</p></div>';
+            echo '<div class="ui-widget-drag" data-key = "'.$key.'" ><p><a href="index.php?proj_idx='.$key.'"'.'>'.$projects["project_id"] .'</a></p></div>';
+        }
+        ?>
+    </div>
+
+
 <script>
 function sortTable(n){
 		var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
@@ -350,6 +410,21 @@ function sortTable(n){
 $(document).ready(function(){
 	sortTable(1);
 	sortTable(1); //default to Last updated Time
+
+    bindProperties();
+    
+	$(document).on("dblclick",".ui-widget-drop",function(event,ui){
+	  	console.log(this.innerText);
+	  	if($('#'+this.innerText+':visible').length == 0)
+	    	$('#'+this.innerText).css('display','block');
+		else{
+    	 	$('#'+this.innerText).css('display','none');
+        	console.log(this.innerText);
+		}
+	});
+
+
+
 	$(".jump").click(function(){
 
 		if($(this).attr("value") == "config")
@@ -432,8 +507,92 @@ $(document).ready(function(){
 		return false;
 	});
 });
+
+  function bindProperties(){
+      $( ".ui-widget-drag").draggable({
+      cursor: "move",
+      drag: function(event,ui){
+      //  ui.css("z-index", "-1"); //fix frontal input
+      }
+
+    });
+
+    $( ".ui-widget-drop" ).droppable({
+      drop: function( event, ui ) {
+        //var pdata = <?php echo json_encode($ALL_PROJ_DATA);?>;
+        var dropBox_name = $.trim(this.innerText);
+        var dragBox_name = $.trim(ui.draggable[0].innerText);
+        var key = $(ui.draggable[0]).data("key");
+        //if does not exist within folder then render it
+        addProject(key,dragBox_name,dropBox_name);
+        $.ajax({
+          url:  "config_gui_post.php",
+          type:'POST',
+          data: "&dropTag=" + dropBox_name + "&dragTag=" + dragBox_name + "&datakey=" + key,
+          success:function(result){
+            console.log(result);
+          }        
+            //THIS JUST STORES IS 
+          },function(err){
+          console.log("ERRROR");
+          console.log(err);
+        });
+        ui.draggable.hide(350);
+      }//drop
+
+    }); //ui-widget-drop
+  }
+
+  function deleteprompt(){
+      var value = confirm("Are you sure you want to delete this folder?");
+      return value;
+
+  }
+  function CreateFolder(name){
+    if(name)
+    {
+      $("<div class ='ui-widget-drop'><p>"+name+"</p></div>").appendTo("#folderspace");
+      let hiddennode = $("<div class = 'hiddenFolders' id ='"+name+"'></div");
+      $("#folderspace").append(hiddennode);
+      bindProperties();
+      $.ajax({
+        url:"config_gui_post.php",
+        type: 'POST',
+        data: "&folders=" + name,
+        success:function(result){
+          console.log(result);
+        }
+        
+        },function(err){
+          console.log("ERROR");
+          console.log(err);
+        
+      });
+    }//if
+    else
+      alert("Please enter a name for your folder");
+  }//CreateFolder
+
+  
+  
+  function addProject(key,dragBox_name,dropBox_name){
+    let div = document.createElement("div");
+    
+    let p = document.createElement("p");
+    let a = document.createElement("a");
+    a.href = "index.php?proj_idx="+key;
+    a.textContent = dragBox_name;
+    $(p).append(a);
+    
+    div.className = "foldercontents";
+    div.setAttribute("data-key",key);
+    $(div).append(p);
+    let search = document.getElementById(dropBox_name);
+    $(search).append(div);
+  }
+
+
 </script>	
-</body>
 </html>
 <style>
 #box {
@@ -482,4 +641,73 @@ input[readonly]{
 	background:#efefef;
 	color:#999;
 }
+
+
+  .hiddenFolders{
+    display: none;
+
+  }
+  #folderspace{
+    height:170px;
+    display:block;
+  }
+  #workingspace{
+    height:300px;
+    margin:20px;
+  }
+  .ui-widget-drop{
+    width: 122px; height: 115px; padding: 0.5em; 
+    float: left;
+    margin: 10px; text-align: center; 
+    background-image: url('img/FolderClose.svg');
+    border: 1px solid red;
+    background-color: transparent;
+    background-size: 100%;
+    line-height: 600%;
+    background-repeat: no-repeat;
+    font-size: 14;
+    margin:20px;
+
+  }
+  .ui-widget-drag{
+    padding: 0.1em; 
+    float: left; 
+    margin: 5px 5px 10px 0; 
+    text-align: center;
+    background-image: url('img/icons8-star-26.png');
+    background-repeat: no-repeat;
+    border: transparent;
+    height: 30px;
+    width: 125px;
+    font-size: 11px;
+    font-weight: bold;
+    line-height: 180%;
+    border:ridge;
+    border-color: blue;
+    border-width: 2px;
+    background-color: azure;
+  } 
+  .foldercontents{
+    padding: 0.1em; 
+    float: left; 
+    margin: 5px 5px 10px 0; 
+    text-align: center;
+    background-image: url('img/icons8-star-26.png');
+    background-repeat: no-repeat;
+    border: transparent;
+    height: 30px;
+    width: 125px;
+    text-align: center;
+    font-size: 11px;
+    font-weight: bold;
+    line-height: 180%;
+    border:ridge;
+    border-color: blue;
+    border-width: 2px;
+    background-color: lightgreen;
+  } 
+
+  .ui-state-highlight{
+    background: transparent;
+  }
 </style>
