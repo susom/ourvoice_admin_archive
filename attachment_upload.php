0d0711c6552ff95d5c88b3ac2f9bae8bad254226
Irvin Szeto <irvins@stanford.edu>
2018-03-25 00:09:05 -0700

    setting up stub endpoint

--- attachment_upload.php
+++ attachment_upload.php
@@ -1,8 +1,14 @@
 <?php
-// http://ourvoice-projects.med.stanford.edu/attachment_upload.php
+require_once("common.php");
 
+if(isset($_POST["doc"])){
+	$doc = json_decode($_POST["doc"],1);
+	print_rr($doc);
 
-print_r( $_POST );
+// $_id  		= $_POST["doc_id"];
+// $url 		= cfg::$couch_url . "/" . cfg::$couch_users_db . "/" . $_id;
+// $response 	= doCurl($url);
+}
 
 
 echo "RECEIVE THE POST THEN RECREATE A PUT TO DATABASE CURL";
