59876fbabe2e39a5a0883251d663889b5c846977
Irvin Szeto <irvins@stanford.edu>
2018-03-25 09:10:05 -0700

    refining the alternate upload api endpoint

--- attachment_upload.php
+++ attachment_upload.php
@@ -1,15 +1,32 @@
 <?php
 require_once("common.php");
 
-if(isset($_POST["doc"])){
+$response = array();
+if(isset($_POST["doc"]) && isset($_POST["doc_id"])){
+	$_id = $_POST["doc_id"];
 	$doc = json_decode($_POST["doc"],1);
-	print_rr($doc);
+	$url = cfg::$couch_url . "/" . cfg::$couch_users_db . "/" . $_id;
+}elseif(isset($_POST["pdoc"]) && isset($_POST["ph_id"])){
+	$_id = $_POST["ph_id"];
+	$doc = json_decode($_POST["pdoc"],1);
+	$url = cfg::$couch_url . "/" . cfg::$couch_attach_db . "/" . $_id;
+}elseif(isset($_POST["adoc"]) && isset($_POST["au_id"])){
+	$_id = $_POST["au_id"];
+	$doc = json_decode($_POST["adoc"],1);
+	$url = cfg::$couch_url . "/" . cfg::$couch_attach_db . "/" . $_id;
+}else{
+	$response["result"] = "error";
+	$_id = "n/a";
+	$doc = "n/a";
+	$url = "n/a";
+}
+$response["_id"] = $_id;
+$response["doc"] = $doc;
+$response["url"] = $url;
 
-// $_id  		= $_POST["doc_id"];
-// $url 		= cfg::$couch_url . "/" . cfg::$couch_users_db . "/" . $_id;
-// $response 	= doCurl($url);
+if($url != "n/a"){
+	// $response["result"] = doCurl($url);
 }
 
-
-echo "RECEIVE THE POST THEN RECREATE A PUT TO DATABASE CURL";
+print_rr("\r",implode($response));
 exit;
