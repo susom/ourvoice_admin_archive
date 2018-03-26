57b35fd270686aab3128b4e36ec9112253a77982
Irvin Szeto <irvins@stanford.edu>
2018-03-26 16:31:07 -0700

    no to actually uploadnig just yet

--- attachment_upload.php
+++ attachment_upload.php
@@ -6,14 +6,17 @@ if(isset($_POST["doc"]) && isset($_POST["doc_id"])){
 	$_id = $_POST["doc_id"];
 	$doc = json_decode($_POST["doc"],1);
 	$url = cfg::$couch_url . "/" . cfg::$couch_users_db . "/" . $_id;
+
 }elseif(isset($_POST["pdoc"]) && isset($_POST["ph_id"])){
 	$_id = $_POST["ph_id"];
 	$doc = json_decode($_POST["pdoc"],1);
 	$url = cfg::$couch_url . "/" . cfg::$couch_attach_db . "/" . $_id;
+
 }elseif(isset($_POST["adoc"]) && isset($_POST["au_id"])){
 	$_id = $_POST["au_id"];
 	$doc = json_decode($_POST["adoc"],1);
 	$url = cfg::$couch_url . "/" . cfg::$couch_attach_db . "/" . $_id;
+	
 }else{
 	$response["result"] = "error";
 	$_id = "n/a";
@@ -21,8 +24,10 @@ if(isset($_POST["doc"]) && isset($_POST["doc_id"])){
 	$url = "n/a";
 }
 
+print_r($doc);
+
 if($url != "n/a"){
-	$result = doCurl($url,json_encode($doc), "PUT");
-	print_r($result);
+	// $result = doCurl($url,json_encode($doc), "PUT");
+	// print_r($result);
 }
 exit;
