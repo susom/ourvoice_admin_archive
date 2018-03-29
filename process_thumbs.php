c4a873dbe29e8ac0583a2f9aed51ca69963e1fb9
Irvin Szeto <irvins@stanford.edu>
2018-03-29 12:34:51 -0700

    oh i guess ill hardcode the project url for now, since SERVER doesnt contain anything useful

--- process_thumbs.php
+++ process_thumbs.php
@@ -6,6 +6,7 @@ require_once("common.php");
 function getPhotos($view, $keys_array){ //keys array is the # integer of the PrID
     $qs         = !empty($keys_array) ? "?" . http_build_query(array( 'key' => $keys_array )) : "";
     $couch_url  = cfg::$couch_url . "/" . cfg::$couch_attach_db . "/" . "_design/get_photos/_view/".$view.$qs;
+    print_rr($couch_url);
     $response   = doCurl($couch_url);
     return json_decode($response,1);
 }
@@ -16,8 +17,8 @@ function getPhotos($view, $keys_array){ //keys array is the # integer of the PrI
 $response 	= getPhotos("all", []);
 
 // now loop and create thumbnails for them
-$url_path 	= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/";
-print_rr($_SERVER);
+// $url_path 	= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/";
+$url_path 	= "https://ourvoice-projects-dev.med.stanford.edu/";
 $photos 	= $response["rows"];
 if(!empty($photos)){
 	foreach($photos as $photo){
