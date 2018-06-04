f81493444a15cc113af4771d0fb4b235a1573c66
Irvin Szeto <irvins@stanford.edu>
2018-06-04 16:05:44 -0700

    finalizing email messaging , need to write api for redcap email relay, ugh

--- upload_ping.php
+++ upload_ping.php
@@ -10,40 +10,48 @@ require_once "inc/class.mail.php";
 $uploaded_walk_id = isset($_POST["uploaded_walk_id"]) ? $_POST["uploaded_walk_id"] : null;                         
 // $proccesed_thumb_ids    = isset($_POST["proccesed_thumb_ids"]) ?  $_POST["proccesed_thumb_ids"] : null;                  
                                                                                                                    
-if(!empty($uploaded_walk_id)){                                                                                           
-    $post   = json_decode($_POST,1);                                                                                 
-    $_id    = $uploaded_walk_id;                                                                                     
-    $doc    = json_decode($_POST["doc"],1);    
-    
-    // GET PROJECT EMAIL 
-    $email 	= isset($_POST["project_email"]) ? $_POST["project_email"] : false;                                                       
-    $photos = $doc["photos"];
+if(!empty($uploaded_walk_id)){ 
+    $_id                = $uploaded_walk_id;  
+    $email              = isset($_POST["project_email"]) ? $_POST["project_email"] : false;   
+    $temp               = explode("_",$_id);
+    $project_id         = $temp[0];
 
-    // SLEEP GIVE IT TIME TO UPLOAD ATTACHMENTS 
-    // 11 seconds (only?) same amount of time as it takes to get to 83% on phone
-    sleep(11); 
+    $emergency_uploaded = false;
+    $check_attach_urls  = array();
+    if(isset($_POST["doc"])){
+        $doc    = json_decode($_POST["doc"],1);   
+        
+        // GET PROJECT EMAIL 
+        $photos = $doc["photos"];
 
-    // CHECK IF ATTACHMENT IDS ARE ALL EXISTING WITHIN disc_attachments AFTER SLEEP PERIOD
-    $check_attach_urls 	= array();
-    $couch_url = "http://".cfg::$couch_user.":".cfg::$couch_pw."@couchdb:5984";
-    foreach($photos as $i => $photo){
-		$ph_id 			= $_id . "_" . $photo["name"];
-		$photo_attach 	= $couch_url . "/".$couch_attach_db."/" . $ph_id ;
-		$check_attach_urls[$photo_attach] = get_head($photo_attach);
-		if(isset($photo["audios"])){
-			foreach($photo["audios"] as $filename){
-				$aud_id			= $_id . "_" . $filename;
-				$audio_attach 	= $couch_url . "/".$couch_attach_db."/" . $aud_id ;
-				$check_attach_urls[$audio_attach] = get_head($audio_attach);
-			}
-		}
-	}
-    $meta = array("uploaded_walk_id" 	=> $_id  
-    			 ,"email" 				=> $email                                                                       
-                 ,"attachments" 		=> $check_attach_urls
-             );   
+        // SLEEP GIVE IT TIME TO UPLOAD ATTACHMENTS 
+        // 11 seconds (only?) same amount of time as it takes to get to 83% on phone
+        sleep(11); 
 
-    echo json_encode(array("uploaded_walk" => $meta)); 
+        // CHECK IF ATTACHMENT IDS ARE ALL EXISTING WITHIN disc_attachments AFTER SLEEP PERIOD
+        $couch_url = "http://".cfg::$couch_user.":".cfg::$couch_pw."@couchdb:5984";
+        foreach($photos as $i => $photo){
+            $ph_id          = $_id . "_" . $photo["name"];
+            $photo_attach   = $couch_url . "/".$couch_attach_db."/" . $ph_id ;
+            $check_attach_urls[$photo_attach] = get_head($photo_attach);
+            if(isset($photo["audios"])){
+                foreach($photo["audios"] as $filename){
+                    $aud_id         = $_id . "_" . $filename;
+                    $audio_attach   = $couch_url . "/".$couch_attach_db."/" . $aud_id ;
+                    $check_attach_urls[$audio_attach] = get_head($audio_attach);
+                }
+            }
+        }
+
+        $meta = array("uploaded_walk_id"    => $_id  
+                     ,"email"               => $email                                                                       
+                     ,"attachments"         => $check_attach_urls
+                 );   
+        echo json_encode(array("uploaded_walk" => $meta)); 
+    }else{
+        // EMERGENCY UPLOAD SUCCESS
+        $emergency_uploaded = "Notification: [$project_id] Emergency Backup Uploaded";
+    }                                                                                
 
     // EITHER YES OR NO ON PARTIAL UPLOADS
     $failed_uploads = array();
@@ -53,28 +61,35 @@ if(!empty($uploaded_walk_id)){
     	}
     }
 
-    // TODO , CREATE EXTERNAL MODULE IN REDCAP TO ACT AS API TO RELAY EMAILS
-    
-    // $temp           = explode("_",$_id);
-    // $project_id     = $temp[0];
-    // $from_name      = "Stanford Our Voice";
-    // $reply_email    = "irvins@stanford.edu";
+    $from_name      = "Stanford Our Voice";
+    $reply_email    = "irvins@stanford.edu";
+    $email_subject  = count($failed_uploads) ? "Notification: [$project_id] New walk uploaded, possibly missing attachments" : "Notification: [$project_id] New walk uploaded!";
+    $email_subject  = !$emergency_uploaded ? $subject : $emergency_uploaded;
+
     // EMAIL TO PROJECT ADMIN EMAIL, CC ANN,IRVIN, JORDAN 
-	// $msg 		= "Hi Admin,\r\n\r\n";
-	// $msg  	   .= count($failed_uploads) ? "Please check the portal for the following attachments; " . implode(", ", $failed_uploads) . ".  If you find them missing on the portal, you can try to upload again from the app.  Please remember to be on a wifi connection and leave the app open through out the upload process.\r\n" : "\r\n";
-	// $subject 	= count($failed_uploads) ? "Notification: [$project_id] New walk uploaded, possibly missing attachments" : "Notification: [$project_id] New walk uploaded!";
+    $msg_arr        = array();
+    $msg_arr[]      = "Hi $project_id  Administrator,<br>";
+    $msg_arr[]      = "This email is letting you know that a walk with id $_id has been uploaded to your project.<br>";
+    $msg_arr[]      = "Please check the <a href='http://ourvoice-projects.med.stanford.edu/summary.php' target='blank' >Our Voice Data Portal</a> to view the walk data.<br>";
+    if(count($failed_uploads)){
+        $msg_arr[]  = "There may have been some technical issues uploading the data resulting in the following attachment files not being fully uploaded:";
+        $msg_arr[]  = "<ul>";
+        $msg_arr[]  = "<li>" . implode("</li>\r\n<li>", $failed_uploads) . "</li>";
+        $msg_arr[]  = "</ul>";
+        $msg_arr[]  = "If you find that some or all of them are missing from the portal, you can try to reset and upload that walk again from the device.";
+        $msg_arr[]  = "Please remember to be on a stable wifi connection and leave the app open through out the upload process.<br>";
+    }
+    $msg_arr[]      = "Cheers!";
+    $msg_arr[]      = "The Our Voice Team";
+    $msg_arr[]      = "<i style='font-size:77%;'>Participant rights: contact our IRB at 1-866-680-2906</i>";
+    $email_msg      = implode("<br>",$msg_arr);
+
+    // TODO , CREATE EXTERNAL MODULE IN REDCAP TO ACT AS API TO RELAY EMAILS
 	// // send email
-	// mail("irvins@stanford.edu", $subject, $msg);                                                              
+	// mail("irvins@stanford.edu", $subject, $msg); 
+    // emailNotification($from_name, $reply_email, $email, $email_subject, $email_msg);                                                              
 }else{
     phpinfo();
-    // mail("irvins@stanford.edu", "TEST", "TESTING TESTING");    
-
-    // $from_name      = "Stanford Our Voice";
-    // $reply_email    = "irvins@stanford.edu";
-    // $email          = "irvins@stanford.edu";
-    // $email_subject  = "testing testing";
-    // $email_msg      = "msg msg";
-    // emailNotification($from_name, $reply_email, $email, $email_subject, $email_msg);
 }
 
 // if get head = succesful
