1e0530e86214b491a77eb91ca6f0de288905105e
Irvin Szeto <irvins@stanford.edu>
2018-10-03 22:36:56 -0700

    updating transcriptions page

--- project_transcriptions.php
+++ project_transcriptions.php
@@ -31,53 +31,99 @@ $active_pid 		= $_GET["pid"];
 <script type="text/javascript" src="https://maps.google.com/maps/api/js?key=<?php echo cfg::$gmaps_key; ?>"></script>
 <script type="text/javascript" src="js/dt_summary.js?v=<?php echo time();?>"></script>
 <style>
-h4[data-toggle="collapse"]{
+h1{
+	margin-bottom:30px;
+}
+h5{
 	padding-bottom:5px;
-	margin-bottom:20px;
+	margin-bottom:10px;
 	border-bottom:1px solid #999;
 	cursor:pointer;
-	font-size:250%;
+	font-size:120%;
 	font-weight:normal;
+}
+
+dl { 
+	margin:0 0 30px; 
+	overflow:hidden; 
+}
+dt { 
+	float:left;  
+	width:148px;
+}
+dt a { 
+	display:block; 
+	border:1px solid #ccc;
+	text-align:center;
+	padding:3px;
+}
+dd { 
+	float:right; 
+	text-align:left;
+	clear:left; 
+	clear:right;
+	margin:0 0 5px;
+	width:calc(100% - 168px);
 }
 </style>
 </head>
 <body id="main">
 <a href="summary.php">Back to Summary</a>
 <?php
-echo "<pre>";
 if( $active_project_id ){
 	//FIRST GET JUST THE DATES AVAILABLE IN THIS PROJECT
     $response 		= filter_by_projid("transcriptions","[\"$active_pid\"]");
 	$project_meta 	= $ap["project_list"][$active_pid];
 
-	//PRINT TO SCREEN
+	$html_buffer 	= array();
 	echo "<h1>Transcription data for project : $active_project_id</h1>";
-
+	
 	foreach($response["rows"] as $sesh){
-		echo "<h4>".$sesh["id"]."</h4>";
-		$transx = $sesh["value"]["transcriptions"];
+		$temp_buffer = array();
 
-		$photo 	= array(); 
-		foreach($transx as $audio_key => $trans){
-			$split 		= explode("_",$audio_key);
-			$photo_key 	= $split[1];
-			if(!array_key_exists($photo_key, $photo)){
-				$photo[$photo_key] = array();
-			} 
-			$photo[$photo_key][$audio_key] = $trans;
-		}
+	    unset($sesh["value"]["project_id"]);
+	    unset($sesh["value"]["_id"]);
+	    unset($sesh["value"]["_rev"]);
+	    unset($sesh["value"]["user_id"]);
+	    unset($sesh["value"]["geotags"]);
+	    unset($sesh["value"]["survey"]);
+	    unset($sesh["value"]["currentDistance"]);
+	    unset($sesh["value"]["upload_try"]);
 
-		foreach($photo as $photo_key => $transx){
-			echo "<h5>Photo_$photo_key</h5>";
-			echo "<ul>";
-			foreach($transx as $audio_key => $trans){
-				echo "<li>$audio_key : $trans</li>";
+	    $lang 			= $sesh["value"]["lang"];
+	    $id_a 			= substr($sesh["id"],0,strlen($sesh["id"]) - 4);
+	    $walk_id 		= substr($sesh["id"],-4);
+		$temp_buffer[] 	=  "<h5>Walk ID: $id_a<b><u>$walk_id</u></b></h5>";
+		foreach($sesh["value"]["photos"] as $photo_key => $photo){
+			$filename 	= $photo["name"];
+			$img_id 	= $ph_id = $sesh["id"] . "_" . $filename;
+
+			$file_uri   = "passthru.php?_id=".$ph_id."&_file=$filename";
+	        $thumb_uri  = "thumbnail.php?file=".urlencode($file_uri)."&maxw=140&maxh=140";
+	        $photo_uri  = getThumb($img_id,$thumb_uri,$file_uri);
+	        $detail_url = "photo.php?_id=".$sesh["id"]."&_file=$filename";
+
+			$temp_buffer[] = "<dl>";
+			$temp_buffer[] = "<dt><a href='$detail_url'><img src='$photo_uri'/></a><dt>";
+			if(isset($photo["audios"])){
+				foreach($photo["audios"] as $key => $audio_key){
+					$transcription = isset($sesh["value"]["transcriptions"]) && isset($sesh["value"]["transcriptions"][$audio_key]) ? $sesh["value"]["transcriptions"][$audio_key]["text"] : "Not transcribed yet...";
+					$temp_buffer[] = "<dd>Audio ".($key+1)." ($lang): " . $transcription ."</dd>";
+				}
 			}
-			echo "</ul>";
+			$temp_buffer[] = "</dl>";
 		}
 
-		echo "<hr>";
+		$last_ 	= strrpos($sesh["id"],"_");
+		$ts 	= substr($sesh["id"], $last_+1);
+		$html_buffer[$ts] = implode("\n",$temp_buffer);
 	}
+
+	krsort($html_buffer);
+
+	foreach($html_buffer as $walk){
+		echo $walk;
+	};
 }
 ?>
 <script>
