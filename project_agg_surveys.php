de44a006faa69c88d6dcf94822f78243dd04920f
Irvin Szeto <irvins@stanford.edu>
2018-12-12 07:16:28 -0800

    making all surveys serve up a CSV

--- project_agg_surveys.php
+++ project_agg_surveys.php
@@ -18,32 +18,13 @@ $projs 				= $ap["project_list"];
 $active_project_id 	= $_GET["active_project_id"];
 $active_pid 		= $_GET["pid"];
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
-<link href="css/dt_common.css?v=<?php echo time();?>" rel="stylesheet" type="text/css"/>
-<link href="css/dt_summary.css?v=<?php echo time();?>" rel="stylesheet" type="text/css"/>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=<?php echo cfg::$gmaps_key; ?>"></script>
-<script type="text/javascript" src="js/dt_summary.js?v=<?php echo time();?>"></script>
-<style>
-h4[data-toggle="collapse"]{
-	padding-bottom:5px;
-	margin-bottom:20px;
-	border-bottom:1px solid #999;
-	cursor:pointer;
-	font-size:250%;
-	font-weight:normal;
-}
-</style>
-</head>
-<body id="main">
-<a href="summary.php">Back to Summary</a>
-<?php
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=surveys_'.$active_project_id.'.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
 if( $active_project_id ){
 	//FIRST GET JUST THE DATES AVAILABLE IN THIS PROJECT
     $response 		= getAggSurveys("[\"$active_pid\"]");
@@ -67,28 +48,30 @@ if( $active_project_id ){
 		}
 	}
 
-	//PRINT TO SCREEN
-	echo "<h1>Aggregate survey data for project : $active_project_id</h1>";
+	array_unshift($headers,"walk id");
 
-	$rows = array();
-	foreach($response["rows"] as $sesh){
+	// output the column headings
+    fputcsv($output, $headers);
+
+    $rows = array();
+    foreach($response["rows"] as $sesh){
 		$row 			= array();
 		$prep_values 	= array();
 		foreach($sesh["value"] as $qa){
 			$prep_values[$qa["name"]] = $qa["value"];
 		}
 
-		$row[] = "<td>".substr($sesh["id"],-4)."</td>";
+		$row[] = substr($sesh["id"],-4);
 		foreach($survey_qa as $qname => $q){
 			if(array_key_exists($qname, $prep_values)){
 				$survey_qa[$qname]["answers"][] = $prep_values[$qname];
 				$word 	=  array_key_exists("options",$survey_qa[$qname]) ? $survey_qa[$qname]["options"][$prep_values[$qname]] : $prep_values[$qname];
-				$row[] 	= "<td>".$word." (".$prep_values[$qname].")</td>";
+				$row[] 	= $word." (".$prep_values[$qname].")";
 			}else{
-				$row[] 	= "<td>n/a</td>";
+				$row[] 	= "n/a";
 			}
 		}
-		$rows[] = "<tr>". implode("",$row) . "</tr>";
+		$rows[] = $row;
 	}
 
 	$summavg = array();
@@ -96,47 +79,19 @@ if( $active_project_id ){
 		$sum = !empty($q["answers"]) ? array_sum($q["answers"]) : 0;
 		$cnt = !empty($q["answers"]) ? count($q["answers"]) : 0;
 		if(!empty($q["answers"]) && is_numeric($q["answers"][0]) ){
-			$summavg[] = "<td>". number_format( $sum/$cnt, 2, ".", "") . " ($cnt responses)</td>";
+			$summavg[] = number_format( $sum/$cnt, 2, ".", "") . " ($cnt responses)";
 		}else{
-			$summavg[] = "<td>n/a</td>";
+			$summavg[] = "n/a";
 		}
 	}
 
-	echo "<table id='survey_agg'>";
-	echo "<thead><tr>";
-	echo "<th>Walk Id</th><th>".implode("</th><th>",$headers)."</th>";
-	echo "</tr></thead>";
-	echo "<tbody>";
-	echo implode("\n",$rows);
-	echo "</tbody>";
-	echo "<tfoot><tr>";
-	echo "<td>Average</td>" . implode("",$summavg);
-	echo "</tr></tfoot>";
-	echo "</table>";
+	array_unshift($summavg,"Average");
+    $rows = array_merge($rows,array($summavg));
+	foreach($rows as $surveys){
+		fputcsv($output, $surveys);
+    }
 }
 ?>
-<style>
-#survey_agg {
-	border-top:1px solid #000;
-	border-right:1px solid #000;
-	width:1200px;
-}
-#survey_agg td, #survey_agg th{
-	min-width:200px;
-	border-left:1px solid #000;
-	border-bottom:1px solid #000;
-	padding:2px 4px;
-	vertical-align: top;
-}
-thead, tfoot{
-	background:#ffffcc;
-}
-tbody {
-
-}
-</style>
-</body>
-</html>
 
 
 
