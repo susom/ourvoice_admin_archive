4cbd118ff133ad097d7fdfb6e784c5f2d27d0c05
Irvin Szeto <irvins@stanford.edu>
2021-04-30 11:20:04 -0700

    sanitizing ajax handler

--- ajaxHandler.php
+++ ajaxHandler.php
@@ -1,9 +1,15 @@
 <?php
 require_once "common.php";
-$request = array_keys($_REQUEST)[0]; //get the request sender
-$data = $_REQUEST[$request]; //get the data associated with directive
+$request 	= array_keys($_REQUEST)[0]; //get the request sender
+$data 		= $_REQUEST[$request]; //get the data associated with directive
 
 if(!empty($request)){
+	$args = array(
+	    'url'   => FILTER_SANITIZE_URL,
+	    'lang'	=> FILTER_SANITIZE_STRING
+	);
+	$data = filter_var_array($data, $args);
+	
     switch($request){
         case 'convertAudio':
             if(!empty($data['url']))  
