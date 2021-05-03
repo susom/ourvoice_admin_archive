f802828f676a0f36ee23771437a93306e66d8b95
Irvin Szeto <irvins@stanford.edu>
2021-05-03 15:51:50 -0700

    fuck

--- ajaxHandler.php
+++ ajaxHandler.php
@@ -4,7 +4,7 @@ require_once "common.php";
 if(!empty($_POST["action"])){
     $request    = filter_var($_POST["action"], FILTER_SANITIZE_STRING);
 	$url        = !empty($_POST["url"])     ? filter_var($_POST["url"], FILTER_SANITIZE_ENCODED) :null;
-    $lang       = !empty($_POST["lang"])    ? filter_var($_POST["lang"], FILTER_SANITIZE_STRING) :null:;
+    $lang       = !empty($_POST["lang"])    ? filter_var($_POST["lang"], FILTER_SANITIZE_STRING) :null;
 	
     switch($request){
         case 'convertAudio':
