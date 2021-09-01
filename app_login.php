bd57afcb33d983b54ead485a30eabe0c8c85331a
Irvin Szeto <irvins@stanford.edu>
2021-08-31 21:03:13 -0700

    stand alone firestore check for projects

--- app_login.php
+++ app_login.php
@@ -18,39 +18,32 @@ $gcp_bucketID       = cfg::$gcp_bucketID;
 $gcp_bucketName     = cfg::$gcp_bucketName;
 $access_token       = getGCPRestToken($keyPath, $firestore_scope);
 
-function restFireStore($firestore_url, $json_payload, $access_token){
+function restGetFireStore($firestore_url, $json_payload, $access_token){
     $curl               = curl_init($firestore_url);
 
     curl_setopt($curl, CURLOPT_HTTPHEADER, array( "Content-Type: application/json"
         , "Authorization: Bearer $access_token"
-        , "Content-Length: " . strlen($json_payload)
-        , "X-HTTP-Method-Override: PATCH"
+        , "X-HTTP-Method-Override: GET"
         )
     );
     curl_setopt($curl, CURLOPT_USERAGENT, "cURL");
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $json_payload);
-    // $getinfo     = curl_getinfo($curl);
-    // $error       = curl_error($curl);
     $response   = curl_exec($curl);
     curl_close($curl);
-
     return $response;
 }
 
 function loginProject($project_id, $project_pass){
-    $result = null;
+    $result = array();
 
-    if(isset($proj_id) && isset($project_pass)){
-
+    if(isset($project_id) && isset($project_pass)){
         $firestore_url  = "https://firestore.googleapis.com/v1/projects/$gcp_project_id/databases/(default)/documents/$projects_data/$project_id";
-        $response       = restFireStore($firestore_url, null, $access_token);
-
-        $snapshot   = $docRef->snapshot();
-        if ($snapshot->exists()) {
-            $data = $snapshot->data();
-            if($data["summ_pass"] == $project_pass || $project_pass == "annban"){
+        $response       = restGetFireStore($firestore_url, null, $access_token);
+        $data           = json_decode($response,1);
+        if(array_key_exists("fields",$data) && isset($data["fields"]["summ_pass"])) {
+            $fs_pw = $data["fields"]["summ_pass"]["stringValue"];
+            if ($fs_pw == $project_pass || $project_pass == "annban") {
                 $result = $data;
             }
         }
