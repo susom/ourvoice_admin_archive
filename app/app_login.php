47146b6fb9887c8f1c4a971af7b818786220d901
Irvin Szeto <irvins@stanford.edu>
2021-09-01 17:10:29 -0700

    this will return a firestore snapshot

--- app/app_login.php
+++ app/app_login.php
@@ -3,15 +3,11 @@ require_once "common.php";
 
 //POST LOGIN TO PROJECT
 $project_snapshot = array();
-if(isset($_POST["proj_id"]) && isset($_POST["proj_pw"]) || 1){
+if(isset($_POST["proj_id"]) && isset($_POST["proj_pw"]) ){
 		$proj_id            = trim(strtoupper(filter_var($_POST["proj_id"], FILTER_SANITIZE_STRING)));
         $proj_pw            = filter_var($_POST["proj_pw"], FILTER_SANITIZE_STRING);
-
-        $proj_id = "AAAA";
-        $proj_pw = "annban";
 
         $project_snapshot   = $ds->loginProject($proj_id, $proj_pw);
 }
 echo json_encode($project_snapshot);
-
 ?>
