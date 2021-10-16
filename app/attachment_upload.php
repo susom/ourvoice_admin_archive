f7ebb6ce50669b336541056c9be8db8fc56b7700
Irvin Szeto <irvins@stanford.edu>
2021-10-15 21:22:53 -0700

    something is wrong with chunked uploads.

--- app/attachment_upload.php
+++ app/attachment_upload.php
@@ -50,7 +50,7 @@ if( isset($_REQUEST["walk_id"]) ){
     $options = array('overwrite' => true);
     $upload_handler = new UploadHandler($options,true,null,$local_folder);
 
-    echo json_encode($upload_handler) ;
+    echo json_encode(array("what happened in attachment_upload?" => $upload_handler)) ;
     exit;
 }
 
